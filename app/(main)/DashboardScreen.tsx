b0a97c859a6ccf810f3d838e86f1c670b6e47387
mateo.montesinos.j <mateo.montesinos.j@gmail.com>
2025-10-08 20:31:44 +0000

    Color commit: Cambiar el color de las cabezas

--- app/(main)/DashboardScreen.tsx
+++ app/(main)/DashboardScreen.tsx
@@ -1,12 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const DashboardScreen = () => {
   return (
-    <View>
+    <SafeAreaView className='bg-black flex-1 justify-center items-center'>
       <Text>DashboardScreen</Text>
       
-    </View>
+    </SafeAreaView>
   )
 }
 
